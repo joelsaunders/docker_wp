f86451d6bd67a487cc0c07b74c7bd0c7d6762717
joel saunders <joelsaunders@ueni.com>
2017-03-06 23:22:22 +0000

    integrate react

---
+++ assets/js/components/app.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return <div>SHittgfdyshittyshit app</div>
+    }
+}
+
+export default App
--- docker_wp/configs/base.py
+++ docker_wp/configs/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
@@ -123,5 +124,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "../assets"),
 )
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, '../webpack-stats.json'),
+    }
+}
