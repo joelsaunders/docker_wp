b66ca0fe94026a353f87c3a016506836fe76f8b5
joel saunders <joelsaunders@ueni.com>
2017-03-27 20:41:29 +0100

    completely seperate them

---
+++ backend_api/docker_wp/configs/override.py
@@ -0,0 +1,7 @@
+from .development import *
+
+DATABASES['default']['NAME'] = 'docker_wp'
+DATABASES['default']['USER'] = 'administrator'
+DATABASES['default']['PASSWORD'] = 'Knebworth1990'
+DATABASES['default']['HOST'] = '0.0.0.0'
+
