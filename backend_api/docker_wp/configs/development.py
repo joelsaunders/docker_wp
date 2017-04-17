65df205ab1274dcda263a271f773553e7693240f
joel saunders <joelsaunders@ueni.com>
2017-04-17 23:12:49 +0100

    make changes to configs

--- backend_api/docker_wp/configs/development.py
+++ backend_api/docker_wp/configs/development.py
@@ -1,14 +1,3 @@
 from .base import *
 
 DEBUG = True
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'postgres',
-        'PORT': 5432,
-    }
-}
--- backend_api/docker_wp/configs/production.py
+++ backend_api/docker_wp/configs/production.py
@@ -1,9 +1,3 @@
-import dj_database_url
 from .base import *
-
-SELF_HOST = 'https://joelsaunders.herokuapp.com/'
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 DEBUG = False
