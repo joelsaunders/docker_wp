231f11a1a2d29a91bcea6e3aa12e385b23cc878d
joel saunders <joelsaunders@ueni.com>
2017-04-18 22:07:35 +0100

    add script for frontend build

--- build_frontend.sh
+++ build_frontend.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
+export commit=`git rev-parse --short HEAD`
+
+export version=`cat frontend/version.txt`
+
+export tag="eu.gcr.io/beaming-might-163819/frontend-$version-$commit"
+
+docker build -t $tag ./frontend
+
+rm -rf nginx/www
+
+mkdir -p nginx/www/src
+
+cp -a frontend/src/style nginx/www/src
+cp -a frontend/index.html nginx/www
+
+docker run --entrypoint cat --rm $tag /code/dist/bundle.js > nginx/www/bundle.js
+
+echo "build finished"
+echo "$tag"
