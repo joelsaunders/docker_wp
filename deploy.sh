24a96949c758b9321c3d73e85f3fba57e93b3c60
joel saunders <joelsaunders@ueni.com>
2017-04-22 17:52:56 +0100

    fix syntax

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-if ["$1" == "deploy"]; then
-    echo "Deploying to prod"
-    # push images
-    
+if ["$DEPLOY" == "deploy"]; then
+    sudo $GSDK docker -- push $FRONTEND_TAG &&
+    sudo $GSDK docker -- push $BACKEND_TAG &&
+    sudo $GSDK docker -- push $NGINX_TAG &&
+    sed -i "s#\"image\": \"backend\",#\"image\": \"${FRONTEND_TAG}\",#" kubernetes/deployment.json &&
+    sed -i "s#\"image\": \"nginx\",#\"image\": \"${FRONTEND_TAG}\",#" kubernetes/deployment.json
 fi
-
