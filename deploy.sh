8d5198819e07a1903dc0a9add61ec880a81da515
joel saunders <joelsaunders@ueni.com>
2017-04-22 18:36:48 +0100

    add correct tags

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,12 @@
 set -e
 
 if [ "$1" == "deploy" ]; then
+    echo "Pushing images to gcloud registry"
     sudo $GSDK docker -- push $FRONTEND_TAG &&
     sudo $GSDK docker -- push $BACKEND_TAG &&
     sudo $GSDK docker -- push $NGINX_TAG &&
-    sed -i "s#\"image\": \"backend\",#\"image\": \"${FRONTEND_TAG}\",#" kubernetes/deployment.json &&
-    sed -i "s#\"image\": \"nginx\",#\"image\": \"${FRONTEND_TAG}\",#" kubernetes/deployment.json
+    echo "adding tags to kubectl"
+    sed -i "s#\"image\": \"backend\",#\"image\": \"${BACKEND_TAG}\",#" kubernetes/deployment.json &&
+    sed -i "s#\"image\": \"nginx\",#\"image\": \"${NGINX_TAG}\",#" kubernetes/deployment.json
+    kubectl apply -Rf kubernetes
 fi
