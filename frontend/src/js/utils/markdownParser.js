d762d188a6fa4f53adb8b3b79296ec8e62cd2498
joel saunders <joelsaunders@ueni.com>
2017-04-24 22:35:59 +0100

    add style for code

--- frontend/src/js/utils/markdownParser.js
+++ frontend/src/js/utils/markdownParser.js
@@ -1,13 +1,23 @@
-import marked from 'marked';
+import marked, { Renderer } from 'marked';
 import hljs from 'highlight.js';
 
+const renderer = new Renderer();
+renderer.code = (code, language) => {
+  // Check whether the given language is valid for highlight.js.
+  const validLang = !!(language && hljs.getLanguage(language));
+  // Highlight only if the language is valid.
+  const highlighted = validLang ? hljs.highlight(language, code).value : code;
+  // Render the highlighted code with `hljs` class.
+  return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
+};
+
 marked.setOptions({
-	renderer: new marked.Renderer(),
+	renderer: renderer,
 	gfm: true,
 	tables: true,
 	breaks: false,
 	pedantic: false,
-	sanitize: true,
+	sanitize: false,
 	smartLists: true,
 	smartypants: false,
     highlight: (code) => {
