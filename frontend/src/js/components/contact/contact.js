b9b9761c6e9f4052cf92fe8c39bbe63b6998a84f
joel saunders <joelsaunders@ueni.com>
2017-05-09 23:26:21 +0100

    add contact page

--- frontend/src/js/components/contact/contact.js
+++ frontend/src/js/components/contact/contact.js
@@ -3,8 +3,27 @@ import React from 'react';
 export default () => {
     return (
         <div>
-            <h3>Contact</h3>
-            <img src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494195241/vovooo13gifSupercompressed_koala-construct3_a9uywt.gif" />
+             <div className="about-biocard generic-card">
+                <div className="about-biotext">
+                    <h3>Get in touch!</h3>
+                    <p>Contact me with any questions or queries you might have about topics in this blog</p>
+                </div>
+                <div className="about-biotable">
+                    <table>
+                        <tbody>
+                            <tr>
+
+                                <td><a href="#"><i className="fa fa-linkedin"></i></a></td>
+                                <td><a href="#"><i className="fa fa-github"></i></a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="#"><i className="fa fa-envelope"></i></a></td>
+                                <td><a href="#"><i className="fa fa-skype"></i></a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/js/components/misc/misc.js
+++ frontend/src/js/components/misc/misc.js
@@ -5,6 +5,7 @@ export default () => {
         <div>
             <h3>Misc</h3>
             <img src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494194581/Atlantis8044constructionheavy_ibiija.gif" />
+            <img src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494195241/vovooo13gifSupercompressed_koala-construct3_a9uywt.gif" />
         </div>
     );
 }
