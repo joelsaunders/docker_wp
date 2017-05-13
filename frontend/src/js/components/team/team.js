ecd561e73e7c29735db1aa5309bd6ed350ac933d
joel saunders <joelsaunders@ueni.com>
2017-05-13 17:44:16 +0100

    add first draft of site page

--- frontend/src/js/components/team/team.js
+++ frontend/src/js/components/team/team.js
@@ -1,76 +1,24 @@
 import React from 'react';
+import TeamTable from './team_table_card';
 
 export default () => {
     return (
         <div>
             <div className="team-tablecontainer generic-card">
                 <div className="detail-post-textarea">
-                    <h3>Team</h3>
+                    <h3>Our team</h3>
                     <p className="contact-text">Our team is our strength. We are one team. We never let one another down. We all work together toward the same goal.</p>
                 </div>
-                <div className="team-table">
-                    <div className="team-table-col">
-                        <div className="team-table-cell">
-                            <div className="headshot-container">
-                                <img className="team-table-picture" src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494528759/12107892_10156064054045526_4654662208648396258_n_nhzpdy.jpg" />
-                                <p><strong>Back-end Developer</strong><br/>Joel Saunders</p>
-                            </div>
-                        </div>
-                        <div className="team-table-cell">
-                            <div className="headshot-container">                            
-                                <img className="team-table-picture" src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494528893/10384751_10155235412420526_8922016018664635108_n_ubjkqk.jpg" />
-                                <p><strong>Joel Force One Pilot</strong><br/>Joel Saunders</p>
-                            </div>
-                        </div>
-                        <div className="team-table-cell">
-                            <div className="headshot-container">
-                                <img className="team-table-picture" src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494528843/13510998_1238570646153379_5610182370510387014_n_jsluoq.jpg" />
-                                <p><strong>Head Chef</strong><br/>Joel Saunders</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="team-table-col">
-                        <div className="team-table-cell">
-                            <div className="headshot-container">                            
-                                <img className="team-table-picture" src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494528817/13939456_10157217881185526_4327777202494721174_n_obrdpq.jpg" />
-                                <p><strong>Front-end Developer</strong><br/>Joel Saunders</p>
-                            </div>
-                        </div>
-                        <div className="team-table-cell">
-                            <div className="headshot-container">                            
-                                <img className="team-table-picture" src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494177669/joelpic1_gobweb.jpg" />
-                                <p><strong>Janitor</strong><br/>Joel Saunders</p>
-                            </div>
-                        </div>
-                        <div className="team-table-cell">
-                            <div className="headshot-container">
-                                <img className="team-table-picture" src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494528870/15747390_10155086779899410_1032417764450909787_n_m3yl6b.jpg" />
-                                <p><strong>CEO, CTO, CFO, FBI</strong><br/>Joel Saunders</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="team-table-col">
-                        <div className="team-table-cell">
-                            <div className="headshot-container">
-                                <img className="team-table-picture" src="http://res.cloudinary.com/dceeo2a79/image/upload/v1494530180/11009107_10155235419370526_7023342185511609798_n_stdgnj.jpg" />
-                                <p><strong>Devops Engineer</strong><br/>Joel Saunders</p>
-                            </div>
-                        </div>
-                        <div className="team-table-cell">
-                            <div className="headshot-container">
-                                <img className="team-table-picture" src="https://res.cloudinary.com/dceeo2a79/image/upload/v1494530245/1016877_10154792592115154_4422530397631181048_n_y08ufu.jpg" />
-                                <p><strong>Joel Saunders</strong><br/>Joel Saunders</p>
-                            </div>
-                        </div>
-                        <div className="team-table-cell">
-                            <div className="headshot-container">
-                                <img className="team-table-picture" src="https://res.cloudinary.com/dceeo2a79/image/upload/v1494530338/20170511_120316_1024_xwbsaq.jpg" />
-                                <p><strong>Chief of Meetings</strong><br/>Joel Saunders</p>
-                            </div>
-                        </div>
-                    </div>
+                <TeamTable />
+            </div>
+            <div className="detail-post-container generic-card">
+                <div className="detail-post-textarea">
+                    <h3>Join Us</h3>
+                    <p>We are always looking for the top talent to join our skilled and dedicated team. Help to change the world and make it a better place for all Joel Saunders. If you are interested and think you meet the requirements, head on over to the <a className="hlink" href="/contact">contact page</a> and apply by email.</p>
+                    <p><strong>Requirements:</strong><br/>Joel Saunders</p>
+                    <p><strong>Nice to haves:</strong><br/>Joel Saunders</p>
                 </div>
-            </div>            
+            </div> 
         </div>
     );
 }
